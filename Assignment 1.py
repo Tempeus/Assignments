b215400742755b4610c764f2b051bd437a0de65c
Tempeus <36718822+Tempeus@users.noreply.github.com>
2018-02-21 21:28:31 -0500

    Add files via upload

--- Assignment 1.py
+++ Assignment 1.py
@@ -16,27 +16,27 @@ y = number + 20 #we set the maximum guess value to be 20 more than the number in
 
 keywordsHigh = ["high", "big","large", "great"]
 keywordsLow = ["low", "small",  "tiny", "miniscule", "short", "little", "petite"]
-
+keywordsCorrect = ["right", "correct", "got it", "did it", "congrats", "congratulation", "you won", "I lost", "you win", "I lose"]
 n = random.randint(x,y) #this was used to specify the range of the randomly generated number
 
 winning = False 
 
 while winning == False:
     print(n) #show the player the computer's guess
-    print("Enter 'h' if my guess is too high, 'l' if too low, or 'c' if I am correct")
+    print("Please specify if my guess is too high or too low or if I guessed it correctly")
     rangeAnswer = input()
-
-    if rangeAnswer == "h":
+    lowerRangeAnswer = rangeAnswer.lower()
+    if any(i in rangeAnswer for i in keywordsHigh):
         y = n - 1 #this is so that the computer will not guess a number that is higher than the number that it just guessed
         n = random.randint(x,y) #this is used to specify the range of the randomly generated number with the new range
       
 
-    elif rangeAnswer == "l":
+    elif any(i in rangeAnswer for i in keywordsLow):
         x = n + 1 #this is so hat the computer will not guess a number that is lower than the number that it just guessed
         n = random.randint(x,y) #this is used to specify the range of the randomly generated number with the new range
        
 
-    if rangeAnswer == "c":
+    if any(i in rangeAnswer for i in keywordsCorrect):
         print("Game over, your secret nubmer was:",n)
         winning == True
         break
